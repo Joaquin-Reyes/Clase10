1b4e3a30b3419918e8d1178eb1d884564d063b46
Joaquin Reyes <joaquinreyes2320@gmail.com>
2023-10-09 22:18:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector ("h1")
+var tituloDos = document.querySelector(".letra")
+var body = document.querySelector ("body")
+
+titulo.textContent = "Sol Te Amo"
+tituloDos.style.color = "red"
+
+var color = prompt ("Escriba un color")
+
+body.style.backgroundColor = color
